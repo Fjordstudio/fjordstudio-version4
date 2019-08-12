50f94d44cd89c5181f0382469eebef7ae5a76c95
fjordscene@gmail.com <fjordscene@gmail.com>
2019-08-12 14:09:07 +0200

    Set up base theme

---
+++ themes/fj_studio/footer.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package fjordstudio
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer">
+		<div class="site-info">
+			<span>
+
+				&copy; Fjordstudio, <?php echo date('Y'); ?>
+
+			</span>
+			<span class="sep"> | </span>
+				<?php
+				/* translators: 1: Theme name, 2: Theme author. */
+				printf( esc_html__( 'Theme: %1$s by %2$s.', 'fj_studio' ), 'fj_studio', '<a href="https://fjordstudio.dk">Katrine-Marie Burmeister</a>' );
+				?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
