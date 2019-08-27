c0fbf5e9f9e9a87c1c2b9d51fb213ee5cddfaa00
fjordscene@gmail.com <fjordscene@gmail.com>
2019-08-27 13:53:44 +0200

    Translated search and comments tags

--- themes/fj_studio/inc/template-tags.php
+++ themes/fj_studio/inc/template-tags.php
@@ -79,7 +79,7 @@ if ( ! function_exists( 'fj_studio_entry_footer' ) ) :
 				sprintf(
 					wp_kses(
 						/* translators: %s: post title */
-						__( 'Leave a Comment<span class="screen-reader-text"> on %s</span>', 'fj_studio' ),
+						__( 'Kommenter indlægget<span class="screen-reader-text"> på %s</span>', 'fj_studio' ),
 						array(
 							'span' => array(
 								'class' => array(),
@@ -96,7 +96,7 @@ if ( ! function_exists( 'fj_studio_entry_footer' ) ) :
 			sprintf(
 				wp_kses(
 					/* translators: %s: Name of current post. Only visible to screen readers */
-					__( 'Edit <span class="screen-reader-text">%s</span>', 'fj_studio' ),
+					__( 'Rediger <span class="screen-reader-text">%s</span>', 'fj_studio' ),
 					array(
 						'span' => array(
 							'class' => array(),
