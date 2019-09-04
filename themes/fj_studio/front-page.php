9468f3364e2ac2e957f77229509fc175cafebe74
fjordscene@gmail.com <fjordscene@gmail.com>
2019-09-04 11:11:05 +0200

    Created frontpage slider

--- themes/fj_studio/front-page.php
+++ themes/fj_studio/front-page.php
@@ -39,8 +39,48 @@
 
         $loop = new WP_Query( $args );
 
-        
+        $slideNo = 1;
+      echo '<ul id="frontpage-slider">';
+      while ( $loop->have_posts() ) : $loop->the_post();
 
+      echo '<li>';
+        echo '<a href="#slide' . $slideNo . '">';
+        echo '<a href="' . get_the_permalink() . '">';
+        echo '<img src="' . the_post_thumbnail() . '" alt="">';
+        echo '<div class="slideOverlay">';
+
+        // echo '<pre style="color:#000;">';
+        // var_dump(get_the_category(get_the_ID()));
+        // echo '</pre>';
+
+        $categories = get_the_category(get_the_ID());
+        // echo $categories[0]->cat_name;
+
+        echo '<p class="sliderCategories">CASE | ';
+        foreach($categories as $key => $value){
+          echo '<span>' . $categories[$key]->cat_name . '</span>';
+        }
+        echo '</p>';
+
+        echo '<h2>' . get_the_title() . '</h2>';
+        the_excerpt();
+
+        if(get_field('date_finished')){
+          echo '<p style="color:#aaa;margin-top:40px;"><b>Færdiggjort: </b>' . get_the_date() . '</p>';
+        }
+        if(get_field('stack')){
+          echo '<p style="color:#aaa;"><b>Stack: </b>' . get_field('stack') . '</p>';
+        }
+        /* echo '<p><a style="color:orchid;" href="' . get_the_permalink() . '"><i class="fas fa-link"></i> Se case her</a></p>';
+        echo '</a>'; */
+        echo '</div>';
+      echo '</a></li>';
+
+        $slideNo++;
+      endwhile;
+      echo '</ul>';
+
+      wp_reset_postdata();
      ?>
 
 		</main><!-- #main -->
