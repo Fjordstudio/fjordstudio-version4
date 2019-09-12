12dd1d8952028321a8a85a17a035d356d26db412
fjordscene@gmail.com <fjordscene@gmail.com>
2019-09-12 11:32:43 +0200

    Added show on scroll script

--- themes/fj_studio/js/script.js
+++ themes/fj_studio/js/script.js
@@ -30,3 +30,31 @@ jQuery(document).ready(function(){
   jQuery('.slideOverlay').css('display','block');
 
 });
+
+(function() {
+  var elements;
+  var windowHeight;
+
+  function init() {
+    elements = document.querySelectorAll('.hidden');
+    windowHeight = window.innerHeight;
+  }
+
+  function checkPosition() {
+    for (var i = 0; i < elements.length; i++) {
+      var element = elements[i];
+      var positionFromTop = elements[i].getBoundingClientRect().top;
+
+      if (positionFromTop - windowHeight <= 0) {
+        element.classList.add('fade-in-element');
+        element.classList.remove('hidden');
+      }
+    }
+  }
+
+  window.addEventListener('scroll', checkPosition);
+  window.addEventListener('resize', init);
+
+  init();
+  checkPosition();
+})();
