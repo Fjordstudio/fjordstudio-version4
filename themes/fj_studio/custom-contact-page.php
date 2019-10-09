46f7b5b2e486660d68416fca3ae971bc51b6ca85
fjordscene@gmail.com <fjordscene@gmail.com>
2019-10-09 13:22:37 +0200

    Called new template part

--- themes/fj_studio/custom-contact-page.php
+++ themes/fj_studio/custom-contact-page.php
@@ -13,7 +13,7 @@ get_header();
 		while ( have_posts() ) :
 			the_post();
 
-			get_template_part( 'template-parts/content', 'page' );
+			get_template_part( 'template-parts/content', 'contact' );
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
@@ -29,3 +29,5 @@ get_header();
 <?php
 get_sidebar();
 get_footer();
+
+?>
