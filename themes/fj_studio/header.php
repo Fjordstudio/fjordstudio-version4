7479694d314b341014942bc5656d3927203a4a65
fjordscene@gmail.com <fjordscene@gmail.com>
2020-01-09 22:03:10 +0100

    Added menu close button

--- themes/fj_studio/header.php
+++ themes/fj_studio/header.php
@@ -45,6 +45,10 @@
 				<p class="site-description"><?php echo $fj_studio_description; /* WPCS: xss ok. */ ?></p>
 			<?php endif; ?>
 
+			<div class="header-search">
+				<?php get_search_form(); ?>
+			</div>
+
 			<div class="icon-nav">
 				<p class="search-button">
 					<svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
